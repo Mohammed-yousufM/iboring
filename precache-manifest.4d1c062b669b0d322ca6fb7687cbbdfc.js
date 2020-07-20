e00f0c5524c2e021551644fe472d7069454d9a1a
yousuf <yousuf3592@gmail.com>
2020-07-20 07:51:49 +0530

    Updates

---
+++ precache-manifest.4d1c062b669b0d322ca6fb7687cbbdfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4270ed3048b153aa8df6d5767ed65cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6374fd4a42a9bb1d5635",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2650c790ae4c08ddcbad",
+    "url": "/static/js/2.18de7ed1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.18de7ed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6374fd4a42a9bb1d5635",
+    "url": "/static/js/main.fc03319c.chunk.js"
+  },
+  {
+    "revision": "16d52defd3b39c416d2b",
+    "url": "/static/js/runtime-main.e42ff7ce.js"
+  }
+]);
